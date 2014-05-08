3be89b870879e2db5c983d3e964f507bda40c238
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2014-05-08 09:25:48 +0200

    fixed bug with python3

--- wordlist/wordlist.py
+++ wordlist/wordlist.py
@@ -14,10 +14,10 @@ import os
 
 from collections import OrderedDict
 
-from _util import (str_product,
-                   parse_charset,
-                   progress,
-                   scan_pattern)
+from ._util import (str_product,
+                    parse_charset,
+                    progress,
+                    scan_pattern)
 
 
 class Generator(object):
