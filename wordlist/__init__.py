9a97bffb2b1f8419ff119d610ef5f8596a4524cc
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2014-05-14 18:00:07 +0200

    code linted 9.5 score

--- wordlist/__init__.py
+++ wordlist/__init__.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Wordlist
+
+Generates all possible permutations of a given charset.
+
+Usage:
+>>>import wordlist
+>>>generator = wordlist.Generator('ab')
+>>>for each in generator.generate(1, 2):
+...    print(each)
+a
+b
+aa
+ab
+ba
+bb
+
+>>>import wordlist
+>>>generator = wordlist.Generator('ab')
+>>>for each in generator.generate_with_pattern('@a'):
+...    print(each)
+aa
+ba
+"""
 __title__ = "wordlist"
 __version__ = '1.0.0'
 
